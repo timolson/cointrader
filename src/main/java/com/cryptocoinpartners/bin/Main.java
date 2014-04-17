4d04cc72e7ef2f64c1b201307f42ead7b294cb0b
Tim Olson <tolson@navonpartners.com>
2014-04-16 17:26:15 -0700

    replaced naive trust manager with instructions for installing the btc-e cert

--- src/main/java/com/cryptocoinpartners/bin/Main.java
+++ src/main/java/com/cryptocoinpartners/bin/Main.java
@@ -73,33 +73,8 @@ public class Main
             LoggerFactory.getLogger(Main.class).info(DEFAULT_PROPERTIES_FILENAME+" not found.  Using "+FALLBACK_PROPERTIES_FILENAME+" instead.");
             Config.init(FALLBACK_PROPERTIES_FILENAME,mainParams.definitions);
         }
-        installTrustManager();
         command.run();
     }
 
-
-    private static void installTrustManager() {
-        TrustManager[] trustAllCerts = new TrustManager[] {
-            new X509TrustManager() {
-                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-                public void checkClientTrusted(
-                    java.security.cert.X509Certificate[] certs, String authType) {
-                    }
-                public void checkServerTrusted(
-                    java.security.cert.X509Certificate[] certs, String authType) {
-                }
-            }
-        };
-
-        // Install the all-trusting trust manager
-        try {
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        } catch (GeneralSecurityException e) {
-        }
-    }
 
 }
