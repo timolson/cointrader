30eea7f32ad6d7cb16901296d648f9614fe4e00a
Douggie <douggie@melvilleclarke.com>
2014-11-09 22:13:13 +0000

    * Purge tables before run

--- src/main/java/org/cryptocoinpartners/bin/BacktestRunMode.java
+++ src/main/java/org/cryptocoinpartners/bin/BacktestRunMode.java
@@ -13,6 +13,7 @@ import org.cryptocoinpartners.schema.Holding;
 import org.cryptocoinpartners.schema.Portfolio;
 import org.cryptocoinpartners.schema.StrategyInstance;
 import org.cryptocoinpartners.schema.Transaction;
+import org.cryptocoinpartners.util.PersistUtil;
 import org.cryptocoinpartners.util.Replay;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -31,7 +32,7 @@ public class BacktestRunMode extends RunMode {
 	@Parameter(description = "Strategy name to load", arity = 1, required = true)
 	public List<String> strategyNames;
 	private Context context;
-	private final Instant start = new DateTime(2013, 1, 01, 0, 0, 0, 0, DateTimeZone.UTC).toInstant();
+	private final Instant start = new DateTime(2013, 11, 01, 0, 0, 0, 0, DateTimeZone.UTC).toInstant();
 	private final Instant end = new DateTime(2014, 1, 01, 0, 0, 0, 0, DateTimeZone.UTC).toInstant();
 	//private final Instant start = new DateTime(2014, 9, 9, 23, 0, 0, 0, DateTimeZone.UTC).toInstant();
 	//private final Instant end = new DateTime(2014, 9, 10, 6, 0, 0, 0, DateTimeZone.UTC).toInstant();
@@ -44,8 +45,9 @@ public class BacktestRunMode extends RunMode {
 
 	@Override
 	public void run() {
-		Replay replay = Replay.between(start, end, true);
-		//Replay replay = Replay.all(true);
+		//Replay replay = Replay.between(start, end, true);
+		PersistUtil.purgeTransactions();
+		Replay replay = Replay.all(true);
 		context = replay.getContext();
 		context.attach(XchangeAccountService.class);
 		context.attach(BasicQuoteService.class);
@@ -57,9 +59,9 @@ public class BacktestRunMode extends RunMode {
 			// context.getInjector().getInstance(cls)
 
 		}
-		//context.attach(ReadTicksCsv.class);
 
 		replay.run();
+		//System.exit(0);
 		// todo report P&L, etc.
 	}
 
