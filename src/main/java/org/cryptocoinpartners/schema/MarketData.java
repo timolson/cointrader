eb31c4264e5e462869c7530f43dc833da83d243a
Douggie <douggie@melvilleclarke.com>
2014-11-09 22:16:34 +0000

    * Cascasding persitatnce of markets

--- src/main/java/org/cryptocoinpartners/schema/MarketData.java
+++ src/main/java/org/cryptocoinpartners/schema/MarketData.java
@@ -1,12 +1,10 @@
 package org.cryptocoinpartners.schema;
 
-
-import org.joda.time.Instant;
-
 import javax.annotation.Nullable;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
+import org.joda.time.Instant;
 
 /**
  * @author Tim Olson
@@ -14,29 +12,29 @@ import javax.persistence.ManyToOne;
 @Entity
 public abstract class MarketData extends RemoteEvent {
 
+	protected MarketData(Instant time, @Nullable String remoteKey, Market market) {
+		this(time, Instant.now(), remoteKey, market);
+	}
 
-    protected MarketData(Instant time, @Nullable String remoteKey, Market market) {
-        this(time,Instant.now(),remoteKey,market);
-    }
+	protected MarketData(Instant time, Instant timeReceived, String remoteKey, Market market) {
+		super(time, timeReceived, remoteKey);
+		this.market = market;
+	}
 
+	@ManyToOne(optional = false)
+	//, cascade = { CascadeType.ALL })
+	public Market getMarket() {
+		return market;
+	}
 
-    protected MarketData(Instant time, Instant timeReceived, String remoteKey, Market market) {
-        super(time, timeReceived, remoteKey);
-        this.market = market;
-    }
+	// JPA
+	protected MarketData() {
+		super();
+	}
 
+	protected void setMarket(Market market) {
+		this.market = market;
+	}
 
-    @ManyToOne(optional = false)
-    public Market getMarket() { return market; }
-
-
-    // JPA
-    protected MarketData() {
-        super();
-    }
-
-    protected void setMarket(Market market) { this.market = market; }
-
-
-    private Market market;
+	private Market market;
 }
